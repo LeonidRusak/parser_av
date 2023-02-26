61c0ec1a839d759dbb6a73a9e850d75c07253a91
Leonid_Rusak <116496436+LeonidRusak@users.noreply.github.com>
2023-02-26 15:32:55 +0300

    Add files via upload

--- parser_av.py
+++ parser_av.py
@@ -1,81 +1,60 @@
+import math
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-url = 'https://cars.av.by/filter?brands[0][brand]=989&brands[0][model]=996&brands[0][generation]=1997&' \
-      'transmission_type=2&body_type[0]=3&engine_type[0]=5'
+# Определение количество страниц пагинации в запросе по определенной модели автомобиля
+# В случае получения ошибки при запросе, повторяем сам запрос пока не получим нужный результат
 
-response = requests.get(url)
-bs = BeautifulSoup(response.text, 'html.parser')
+while True:
+    try:
+        url = 'https://cars.av.by/peugeot/307'
+        response = requests.get(url)
+        bs = BeautifulSoup(response.text, 'html.parser')
 
-temp_car = bs.findAll('h3', class_='listing-item__title')  # Нахождение всех объявлений, удовлетворяющих запросу
-data = list(temp_car)
+        car = bs.findAll('h3', class_='listing__title')  # Нахождение количества объявлений, удовлетворяющих запросу
+        count = math.ceil(int(str(car)[52:55]) / 25)  # Количество страниц пагинации по нашему запросу
+        break
+    except Exception:
+        continue
 
-list_id = []  # Список id найденных авто по фильтру на сайте
 
-for i in data:  # Выделение id объявления на сайте и добавление его в список
-    a = str(i)
-    a = a[69:90]
-    list_id.append(a)
+data_id = []  # Список с id найденных объявлений
+data_price = []  # Список с ценами автомобилей в найденных объявлениях
+data_year = []  # Список с годами выпуска автомобилей
+data_mileage = []  # Список с пробегами автомобилей, указанных в объявлении
 
-temp_price = bs.findAll('div', class_='listing-item__priceusd')  # Цена авто
-data = list(temp_price)
+for i in range(1, count+1):
+    url = f'https://cars.av.by/filter?brands[0][brand]=989&brands[0][model]=996&page={i}&sort=4'
+    response = requests.get(url)
+    bs = BeautifulSoup(response.text, 'html.parser')
+    # Нахождение всех id объявлений, удовлетворяющих запросу
+    car_id = bs.findAll('a', class_='listing-item__link')
+    data_id += list(car_id)
+    car_price = bs.findAll('div', class_='listing-item__priceusd')
+    data_price += list(car_price)
+    car_year = bs.findAll('div', class_='listing-item__params')
+    data_year += list(car_year)
+    data_mileage += list(car_year)
 
-list_price = []
-
-for i in data:
-    a = str(i)
-    a = a[38] + a[40:43]
-    list_price.append(a)
-
-temp_year = bs.findAll('div', class_='listing-item__params')  # Год выпуска авто
-data = list(temp_year)
-
-list_year = []
-
-for i in data:
-    a = str(i)
-    a = a[39:43]
-    list_year.append(a)
-
-temp_mileage = bs.findAll('div', class_='listing-item__params')  # Пробег авто
-data = list(temp_mileage)
-
-list_mileage = []
-
-for i in data:
-    a = str(i)
-    a = a[160:163] + a[164:167]
-    list_mileage.append(a)
 
 list_final = []  # Итоговый список с найденными объявлениями
 num_ad = 1  # Номер объявления в полученном списке
 
-for i in list_id:  # Добавление ссылки на авто
-    list_final.append(f'{num_ad}. ' + 'https://cars.av.by/' + i)
+for i in range(len(data_id)):
+    list_final.append(f'{num_ad}.' + ' ' + 'https://cars.av.by/' + str(data_id[i])[37:58] + ' цена: ' +
+                      ''.join([i for i in str(data_price[i])[37:45] if i.isdigit()]) + '$, год выпуска: ' +
+                      str(data_year[i])[39:43] + ', пробег: ' +
+                      ''.join([i for i in str(data_mileage[i])[-29:-19] if i.isdigit()]) + 'км')
     num_ad += 1
 
-count = 0
-for i in list_price:  # Добавление цены авто
-    list_final[count] = list_final[count] + ' цена: ' + i + '$'
-    count += 1
-
-count = 0
-for i in list_year:  # Добавление года выпуска авто
-    list_final[count] = list_final[count] + ' год выпуска: ' + i
-    count += 1
-
-count = 0
-for i in list_mileage:  # Добавление пробега авто
-    list_final[count] = list_final[count] + ' пробег: ' + i + 'км'
-    count += 1
 
 """
-Для удобного понимания информации в генерируемом текстовом файле, в шапку добавим описание.
-Полученный список запишется в файл построчно для удобства чтения 
+Для удобного понимания информации в генерируемом текстовом файле, в шапку добавлено описание.
+Полученный список запишется в файл построчно для удобства чтения
 """
-list_final.insert(0, f'Список актуальных объявлений на {datetime.now()}')
-list_final.insert(1, 'Марка: Peugeot Модель: 307 I Двигатель: Дизель 2.0 КПП: механика Кузов: Хэтчбек 5дв.')
+list_final.insert(0, f'Список актуальных объявлений на {datetime.now().strftime("%d-%m-%Y %H:%M:%S")}')
+list_final.insert(1, 'Марка: Peugeot Модель: 307')
 
 file_text = open('parser.txt', 'w', encoding='utf-8')
 for i in list_final:
